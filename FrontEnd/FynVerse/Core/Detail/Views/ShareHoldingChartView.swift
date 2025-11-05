b25dccecfc39e2ad4e42cbb9bc6d6ff448211e28
zubair0831 <zubairahmed20050831@gmail.com>
2025-11-05 11:06:34 +0530

    Add FynVerse presentation

---
+++ FrontEnd/FynVerse/Core/Detail/Views/ShareHoldingChartView.swift
@@ -0,0 +1,8 @@
+//
+//  ShareHoldingChartView.swift
+//  FynVerse
+//
+//  Created by zubair ahmed on 09/09/25.
+//
+
+import Foundation
---
+++ FrontEnd/FynVerse/Services/StockAnalysisService.swift
@@ -0,0 +1,8 @@
+//
+//  StockAnalysisService.swift
+//  FynVerse
+//
+//  Created by zubair ahmed on 06/10/25.
+//
+
+import Foundation
